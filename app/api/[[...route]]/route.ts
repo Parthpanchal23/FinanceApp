59c2bf85b84e73b4581aec99a6bf4a52982a2639
Parth Panchal <parthpanchal.2023@gmail.com>
2024-07-23 14:59:26 +0530

    account related  api create

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -2,16 +2,28 @@ import { Hono } from "hono";
 import { handle } from "hono/vercel";
 
 export const runtime = "edge";
-import authors from "./authors";
-import books from "./books";
+// import authors from "./authors";
+// import books from "./books";
+import accounts from "./accounts";
+// import { HTTPException } from "hono/http-exception";
 
 const app = new Hono().basePath("/api");
 
-app.get("/hello", (c: any) => {
-  return c.Json({ hwllo: "world" });
-});
+// app.get("/hello", (c: any) => {
+//   return c.Json({ hwllo: "world" });
+// });
 // app.route("/authors", authors);
 // app.route("/books", books);
 
+// app.onError((err, c) => {
+//   if (err instanceof HTTPException) {
+//     return err.getResponse();
+//   }
+//   return c.json({ error: "Internal error" }, 500);
+// });
+const routes = app.route("/accounts", accounts);
 export const GET = handle(app);
 export const POST = handle(app);
+
+// RPC
+export type AppType = typeof routes;
